4ec2238b1bbc2def9a2b1dffb8b74ba1e741b23f
krove <krove@DESKTOP-Q5SED1R>
2020-09-26 00:50:55 +0530

    UC5 added. Calculating wages for a month

--- src/com/capgemini/EmployeeWageBuilderOOP.java
+++ src/com/capgemini/EmployeeWageBuilderOOP.java
@@ -1,25 +1,30 @@
 package com.capgemini;
 
 public class EmployeeWageBuilderOOP {
-	public static final int IS_PART_TIME = 1;
-	public static final int IS_FULL_TIME = 2,EMP_RATE_PER_HOUR = 20;
+	public static final int is_part_time = 1;
+	public static final int is_full_time = 2,emp_rate_per_hour = 20,num_of_working_days = 20;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int empHrs = 0,empWages = 0;
-		int empCheck = (int) Math.floor(Math.random()*10)%3;
-		switch(empCheck) {
-		case IS_PART_TIME:{
-			empHrs = 4;
-			break;
+		int empHrs = 0,empWage = 0,totalEmpWage = 0;
+		for(int day = 0; day < num_of_working_days;day++) {
+			int empCheck = (int) Math.floor(Math.random()*10)%3;
+			switch(empCheck) {
+			case is_part_time:{
+				empHrs = 4;
+				break;
+			}
+			case is_full_time:{
+				empHrs = 8;
+				break;
+			}
+			default:
+				empHrs = 0;
+			}
+			empWage = empHrs*emp_rate_per_hour;
+			totalEmpWage += empWage;
+			System.out.println("Emp Wage: "+empWage);
+			
 		}
-		case IS_FULL_TIME:{
-			empHrs = 8;
-			break;
-		}
-		default:
-			empHrs = 0;
-		}
-		empWages = empHrs*EMP_RATE_PER_HOUR;
-		System.out.println("Emp wage: "+empWages);
+		System.out.println("Total Emp Wage: "+totalEmpWage);
 	}
 }
