e69affb560566571d0c1e2dce0d6e1b1043104ff
krove <krove@DESKTOP-Q5SED1R>
2020-09-27 19:39:05 +0530

    [Kashif]UC13 added. Storing daily wage along with total wage

--- src/com/capgemini/EmployeeWageBuilderOOP.java
+++ src/com/capgemini/EmployeeWageBuilderOOP.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 public class EmployeeWageBuilderOOP implements ComputeEmpWage{
 	public static final int IS_FULL_TIME = 1;
-	public static final int IS_PART_TIME = 2;
+	public static final int IS_PART_TIME = 2;   
 	private ArrayList<CompanyEmpWage> companyEmpWageList;
 	private Map<String, CompanyEmpWage> companyToEmpWageMap;	
     public EmployeeWageBuilderOOP() {
@@ -21,9 +21,11 @@ public class EmployeeWageBuilderOOP implements ComputeEmpWage{
     }
     @Override
 	public void computeEmpWage() {
-		CompanyEmpWage companyEmpWage = companyEmpWageList.get(0);
+    	for(int i = 0; i < companyEmpWageList.size(); i++){
+		CompanyEmpWage companyEmpWage = companyEmpWageList.get(i);
 		companyEmpWage.setTotalEmpWage(this.computeEmpWage(companyEmpWage));
 		System.out.println(companyEmpWage);
+      }
 	}
 	private int computeEmpWage(CompanyEmpWage companyEmpWage) {
 		int empHours = 0, totalEmpHours = 0, totalWorkingDays = 0;
